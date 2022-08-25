c92d902fe8cd6963cb07faeddc3eeb9696dfc1b3
SREEKANTH PS <sreekanth3265@gmail.com>
2022-08-25 10:30:26 +0530

    cashe cleared

--- frontend/src/pages/UserEdit.jsx
+++ frontend/src/pages/UserEdit.jsx
@@ -1,89 +1,50 @@
 import React, { useState ,useEffect} from 'react'
 import { FaUser } from 'react-icons/fa'
+import axios from 'axios'
 import {useNavigate,useParams} from 'react-router-dom'
-import {getUser,reset} from '../features/admin/adminSlice'
-import { editUser } from '../features/admin/adminSlice'
+//import {getUser,reset} from '../features/admin/adminSlice'
+//import { editUser } from '../features/admin/adminSlice'
 import { useSelector,useDispatch } from 'react-redux'
 import { Spinner } from 'react-bootstrap'
 import { toast } from "react-toastify";
 function UserEdit() {
   const navigate=useNavigate()
   const dispatch=useDispatch()
-  const { id } = useParams();
-  console.log(id+'---------------');
-    const {isLoading,isError,message}=useSelector(
-        (state)=>state.auth
-        )
-        const data=useSelector((state)=>
-        state.auth.user
-        )
-        console.log(data._id+'=================');
-        const { user } =useSelector((state)=>{ return state.auth
-        })
-        // console.log(user._id+'=============');
-      
-        const [formData, setFormData] = useState({});
-      
-        useEffect(() => {
-          dispatch(getUser(user));
-        }, []);
+ 
+   const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    phone:''
+   });
+   const [state,setState]=useState([])  
+        
+   const { id } = useParams();
+   console.log(id+'---------------');     
+   useEffect(()=>{
 
-        useEffect(()=>{
-
-        if (!data) {
-      navigate("/login");
-    }
-    // if (data && !data.isAdmin) {
-    //   navigate("/");
-    // }
-    let name = user?.name;
-    let email = user?.email;
-    let phone=user?.phone;
-    setFormData({ name, email,phone });
-  }, [user, navigate]);
-
-        const onSubmit=(e)=>{
-
-          e.preventDefault()
-          let {name,email,phone}=formData
-          
-          const userData = {
-            name,
-            email,
-            phone,
-            id,
-          };
-
-          if (!name) {
-            toast.error("Name is required");
-          } else if (!email) {
-            toast.error("Email is required");
-          }else{
-            dispatch(editUser(userData));
-           // navigate("/admin");
-          }
-
-
-      }
-
+      axios({
+        method:'get',
+        url:`/api/admin/finduser/${id}`
+      }).then((res)=>setFormData(res.data))
+    },[])
     
-    const {name,email,phone}=formData;
+    const { name, email,phone } = formData
 
-    // useEffect(()=>{
-    //     if(isError){
-    //         console.log(message);
-    //       }
-    //     dispatch(getUser())
-    // },[isError,message,user,dispatch])
 
     const onChange=(e)=>{
-        setFormData((prevState)=>({
-           ...prevState,[e.target.name]:e.target.value 
-        }))
-    }
-    if (isLoading) {
-        return <Spinner />;
-      }
+      setFormData((prevState)=>({
+          ...prevState,
+          [e.target.name]:e.target.value
+      }))
+
+  }  
+
+  const onSubmit=async(e)=>{
+    e.preventDefault()
+    const data=await axios.put(`/api/admin/edituser/${id}`,{name,email,phone})
+    navigate('/admin')
+  }  
+  
 
   return (
     <>
